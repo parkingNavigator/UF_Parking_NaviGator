a183112937666c6a2bc832ecf6b93397474589f3
hkw25 <bohaowang@ufl.edu>
2025-04-19 00:47:02 -0400

    add fontFamily: Roboto and Helvetica

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -10,7 +10,7 @@ const theme = createTheme({
         },
       },
       typography: {
-        fontFamily: "Arial, sans-serif",
+        fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
       },    
 })
 
